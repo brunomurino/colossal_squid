2c1a9a54c5272ac361c5b247427d59f582c94f46
Bruno Murino <brunomurino@brunos-mbp.home>
2019-11-17 21:11:14 +0000

    updated main to create bucket and bigquery dataset

---
+++ README.md
@@ -0,0 +1,10 @@
+# Colossal Squid
+
+* Create GCP account
+* Create service-account with editor permissions and download the JSON
+* Create bucket to store terraform state
+* Enable versioning on bucket
+After you install gcloud, run the following command to authenticate your terminal session (https://cloud.google.com/sdk/gcloud/reference/auth/activate-service-account): 
+```gcloud auth activate-service-account --key-file=$GOOGLE_APPLICATION_CREDENTIALS```
+Then, run the following command to enable versioning on your terraform state bucket (https://cloud.google.com/storage/docs/using-object-versioning):
+```gsutil versioning set on gs://colossal-squid-terraform-state-prod```
--- colossal-squid/main.tf
+++ colossal-squid/main.tf
@@ -4,15 +4,24 @@ terraform {
     prefix  = "terraform/state"
     credentials = "colossal-squid-8c6f897419bb.json"
   }
-
-  required_providers {
-    google = "~> 3.0"
-  }
 }
 
+# https://learn.hashicorp.com/terraform/gcp/build
 provider "google" {
   credentials = file("colossal-squid-8c6f897419bb.json")
   project = "colossal-squid"
   region  = "us-central1"
   zone    = "us-central1-c"
+}
+
+resource "google_storage_bucket" "colossal_squid_test_bucket" {
+  name     = "colossal-squid-test-bucket"
+  location = "us-central1"
+}
+
+resource "google_bigquery_dataset" "colossal_squid_db" {
+  dataset_id                  = "colossalsquiddb"
+  friendly_name               = "colossal-squid-db"
+  description                 = "This is a test description"
+  location                    = "US"
 }
